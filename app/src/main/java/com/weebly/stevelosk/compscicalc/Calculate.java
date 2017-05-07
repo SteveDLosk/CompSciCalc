85c3ebbb36fae4efb7ac6758592ca82e06fd240f
Steve Losk <Stevedlosk@gmail.com>
2017-05-06 19:49:15 -0700

    started Calculate method.

---
+++ app/src/main/java/com/weebly/stevelosk/compscicalc/Calculate.java
@@ -0,0 +1,23 @@
+package com.weebly.stevelosk.compscicalc;
+import java.math.BigInteger;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by steve on 5/6/2017.
+ */
+
+public class Calculate {
+
+
+    private BigInteger processIntsString (String s) {
+        /* takes a sting of instructions, splits them into BigIntegers, and processes
+        according to order of operations, and finally returns the result
+         */
+
+        String[] ints = s.split("[+|-|*/]");
+    }
+    List<BigInteger> ints = new ArrayList<BigInteger>();
+
+}
