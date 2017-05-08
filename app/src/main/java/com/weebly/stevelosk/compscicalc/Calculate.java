2d0f7e6d56170de3af43a6e2a7a3e6f6ddfe432d
Steve Losk <Stevedlosk@gmail.com>
2017-05-07 17:51:56 -0700

    Added some math calculating methods

--- app/src/main/java/com/weebly/stevelosk/compscicalc/Calculate.java
+++ app/src/main/java/com/weebly/stevelosk/compscicalc/Calculate.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.weebly.stevelosk.compscicalc.R.string.equals;
+
 /**
  * Created by steve on 5/6/2017.
  */
@@ -11,13 +13,98 @@ import java.util.List;
 public class Calculate {
 
 
-    private BigInteger processIntsString (String s) {
-        /* takes a sting of instructions, splits them into BigIntegers, and processes
-        according to order of operations, and finally returns the result
-         */
+    public String[] processString (String s) {
+        // at this point, only accepts one operator.  Will expand later.
 
-        String[] ints = s.split("[+|-|*/]");
+        // TODO:
+        // pre-process string, remove trailing and leading operators
+        char[] str = s.toCharArray();
+
+        StringBuilder digits = new StringBuilder();
+        String operator = null;
+        String operand1 = "";
+        String operand2;
+
+        for (int i = 0; i < s.length(); i++) {
+            char next = str[i];
+            // if it is a digit
+            if (next >= 48 && next <= 57) {
+                digits.append(next);
+            }
+            else {
+                operator += next;
+                operand1 = digits.toString();
+                // reset for the next operand
+                digits.setLength(0);
+            }
+        }
+        // make second operand
+        operand2 = digits.toString();
+
+        String[] result = new String[3];
+        result[0] = operand1;
+        result[1] = operator.toString();
+        result[2] = operand2;
+        return result;
     }
-    List<BigInteger> ints = new ArrayList<BigInteger>();
+
+    public long calculateString (String[] inputs) {
+        long op1 = Long.parseLong(inputs[0]);
+        long op2 = Long.parseLong(inputs[2]);
+
+        if (inputs[1].equals("+"))
+            return op1 + op2;
+        else if (inputs[1].equals("-"))
+            return op1 - op2;
+        else if (inputs[1].equals("*"))
+            return op1 * op2;
+        else if (inputs[1].equals("/"))
+            return op1 / op2;
+
+        // should be unreachable
+        return Long.MIN_VALUE;
+    }
+
+    public long factorial(long n) {
+
+        // TODO:
+        // handle excessively long-running processes and overflow
+        // by handling too large inputs.  Maybe return -1
+
+        long result = 1;
+        while (n > 1) {
+            result *= n;
+            n--;
+        }
+
+        return result;
+    }
+
+    public long square (long n) {
+        return n * n;
+    }
+    public double square (double n) {
+        return n * n;
+    }
+
+    public long exponent (long n, long x) {
+
+        long result = n;
+        while (x > 1) {
+            result *= n;
+            x--;
+        }
+        return result;
+    }
+
+    public double exponent (double n, double x) {
+
+        double result = n;
+        while (x > 1) {
+            result *= n;
+            x--;
+        }
+        return result;
+    }
 
 }
